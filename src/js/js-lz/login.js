82a02662a0a952a0e6c3c2866aab11a093dbc951
hlpei <302796441@qq.com>
2019-08-20 10:07:19 +0800

    手机验证通过

--- src/js/js-lz/login.js
+++ src/js/js-lz/login.js
@@ -3,19 +3,34 @@ $("a").tap(function () {
 })
 
 // 验证用户输入手机号
-var suc=false;
-$("#u_input").focus(function(){
-   var val= $(this).val();
-   console.log(val);
-   var reg=/[1][3-9][\d]{9}/;
-   if(reg.test(val)){
-       $(".go_next i").addClass("go_next_color");
-       suc=true;
-   }
-//     else{
-//        $(".error_tips").show();
-//    }
+let suc = false;
+let n = 0;
+$("#u_input").keyup(function (e) {
+    n++;
+    if (n == 11) {
+        let val = $(this).val();        
+        let reg = /[1][3-9][\d]{9}/;
+        if (reg.test(val)) {
+            suc = true;
+            $(".go_next i").addClass("go_next_color");
+            $(".error_tips").addClass("disp_n");
+        } else {
+            n = 0;
+            $(".go_next i").removeClass("go_next_color");
+            $(".error_tips").removeClass("disp_n");
+        }
+    }
+    if (!$("#u_input").val()) {
+        n = 0;
+        $(".error_tips").addClass("disp_n");
+        $(".go_next i").removeClass("go_next_color");
+    }
 })
+
+if(suc){
+    
+}
+
 
 
 
