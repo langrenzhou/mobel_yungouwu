72cc0ef0dcc9548f509d0e9abd9f77dbbe53cf10
hlpei <302796441@qq.com>
2019-08-21 12:34:42 +0800

    随机滑块图

--- src/js/js-lz/login.js
+++ src/js/js-lz/login.js
@@ -30,21 +30,53 @@ $("#u_input").keyup(function () {
     }
 
     // 验证通过
-    // 新页面
+
     if (suc) {
-        $(".go_next").tap(function () {
-            window.location.href="./verify_code.html?pho_num="+val;
-        })
+        // 滑块验证
+        slide_verify();
+        // 点击跳到验证码页面
+        if ($(".small_pic").css("left") == pos_left[random_n]) {
+            $(".cover_slide_verify").addClass("disp_n");
+            verify_code();
+        }
+
     }
 })
 
+function slide_verify() {
+    random_slide_pic();
+    $(".cover_slide_verify").removeClass("disp_n");
+    move_slide();
+}
 
+const pos_left = [166, 200, 160];
+let random_n = Math.floor(Math.random() * 3 + 1);
+// let random_html;
+function random_slide_pic() {
+    let random_html = `<img class="bg_pic" src="../../images/image_lz/slide_bg_${random_n}.jpg">
+    <img class="small_pic pa" src="../../images/image_lz/slide_small_${random_n}.png">`
+    $(".slide_pic").html(random_html);
+}
 
+function move_slide() {
+    $(".cover_slide_verify").on("mousemove", function (e) {
+        let _left = e.clienX - $(".small_pic").width() / 2;
+        let _top = e.clienY - $(".small_pic").height() / 2;
+        $(".small_pic").css({
+            left: _left,
+            top: _top
+        })
+    })
 
+}
 
+function verify_code() {
+    $(".go_next").tap(function () {
+        window.location.href = "./verify_code.html?pho_num=" + val;
+    })
+}
 
-
-
+// 
 var mark = true;
 $(".protocol").tap(function () {
     if (mark) {
